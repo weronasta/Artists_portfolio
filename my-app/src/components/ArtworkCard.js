4e44f8ca069753493e5f962c7fbccbe7048a8f92
Weronika Stachurska <weronasta@gmail.com>
2025-01-30 20:57:05 +0100

    Added delete artwork feature

--- my-app/src/components/ArtworkCard.js
+++ my-app/src/components/ArtworkCard.js
@@ -2,8 +2,11 @@ import React from "react";
 import { Box, Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useCart } from "../contexts/CartContext";
+import axios from "axios";
 
-function ArtworkCard({ artwork, cardType }) {
+
+
+function ArtworkCard({ artwork, cardType}) {
   const navigate = useNavigate();
   const { addToCart } = useCart();
 
@@ -18,6 +21,37 @@ function ArtworkCard({ artwork, cardType }) {
   const handleEditWork = () => {
     navigate(`/edit/${artwork.id}`); // Przekierowanie do edytowania pracy
   };
+  const handleDeleteWork = async (id) => {
+    const token = localStorage.getItem("authToken");
+      if (!token) {
+        console.error("No token found");
+        return;
+      }
+    if (window.confirm("Czy na pewno chcesz usunąć tę pracę?")) {
+      // Usunięcie pracy
+      // call to api
+      axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+      try {
+          const response = await axios.delete(`http://127.0.0.1:5000/delete_artwork/${id}`,{});
+          if (response.status === 200) {
+            console.log("Usunięto pracę o id: ", id);
+            window.location.reload();
+          } else {
+            console.error("Error deleting artwork");
+          }
+      }
+      catch (error) {
+          console.error("Error deleting artwork:", error);
+      }
+      
+      
+
+  
+      alert(`Usunięto pracę o id: ${id}`);
+    }
+  };
+
+  
 
   const imageUrl = require(`../assets/images/artworks/${artwork.imageLink}`);
 
@@ -94,6 +128,13 @@ function ArtworkCard({ artwork, cardType }) {
               onClick={handleEditWork}
             >
               Edytuj pracę
+            </Button>
+            <Button
+              variant="contained"
+              color="error"
+              onClick={() => handleDeleteWork(artwork.id)}
+            >
+              Usuń pracę
             </Button>
             <Button
               variant="contained"
